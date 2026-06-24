fbfcd713f71dc80eda694fe181f02311917ac2cc
techsolutionalwar-create <techsolutionalwar@gmail.com>
2026-06-24 14:17:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+
   server: {
     port: 5173,
   },
+
   build: {
     outDir: 'dist',
     sourcemap: false,
     chunkSizeWarningLimit: 2000,
   },
+
   optimizeDeps: {
     include: ['lightweight-charts', 'crypto-js'],
   },
